5b835f2277aae2add0c63d1d9c4ddb63c0c24112
poison <rc.poison@gmail.com>
2018-09-27 21:29:27 +0200

    begin REST facade

--- dxvk-cache-pool-server/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/server/DxvkCachePoolREST.java
+++ dxvk-cache-pool-server/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/server/DxvkCachePoolREST.java
@@ -5,16 +5,20 @@
  */
 package com.ignorelist.kassandra.dxvk.cache.pool.server;
 
+import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableSet;
-import com.ignorelist.kassandra.dxvk.cache.pool.common.FsScanner;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCache;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCacheEntry;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCacheInfo;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.ExecutableInfo;
 import com.ignorelist.kassandra.dxvk.cache.pool.server.storage.CacheStorage;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.util.Set;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -30,18 +34,51 @@ public class DxvkCachePoolREST {
 	@Inject
 	private CacheStorage cacheStorage;
 
-	@GET
-	@Path("executableInfo")
+	@POST
+	@Path("cacheDescriptors/{version}")
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Set<ExecutableInfo> executableInfos() {
-		FsScanner fsScanner=FsScanner.scan(ImmutableSet.of(Paths.get("/home/poison/.wine/")));
-		return fsScanner.getExecutables();
+	public Set<DxvkStateCacheInfo> getCacheDescriptors(@PathParam("version") int version, Set<ExecutableInfo> executableInfos) {
+		if (null==executableInfos) {
+			throw new IllegalArgumentException("missing executableInfos");
+		}
+		return executableInfos.parallelStream()
+				.map(cacheStorage::getCacheDescriptor)
+				.filter(Predicates.notNull())
+				.collect(ImmutableSet.toImmutableSet());
 	}
 
 	@POST
-	@Path("executableInfo")
-	public void executableInfos(Set<ExecutableInfo> executableInfos) {
-		executableInfos.forEach(System.err::println);
+	@Path("stateCache/{version}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public DxvkStateCache getStateCache(@PathParam("version") int version, ExecutableInfo executableInfo) {
+		if (null==executableInfo) {
+			throw new IllegalArgumentException("missing executableInfo");
+		}
+		return cacheStorage.getCache(executableInfo);
+	}
+
+	@POST
+	@Path("missingCacheEntries")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Set<DxvkStateCacheEntry> getMissingEntries(DxvkStateCacheInfo cacheInfo) {
+		if (null==cacheInfo) {
+			throw new IllegalArgumentException("missing cacheInfo");
+		}
+		return cacheStorage.getMissingEntries(cacheInfo);
+	}
+
+	@POST
+	@Path("stateCache")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void store(DxvkStateCache dxvkStateCache) throws IOException {
+		if (null==dxvkStateCache) {
+			throw new IllegalArgumentException("missing dxvkStateCache");
+		}
+		// TODO: validate
+		cacheStorage.store(dxvkStateCache);
 	}
 
 }
