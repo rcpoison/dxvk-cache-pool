b26517783e7ecf098f0aeb8137675a6f0d5c63bb
poison <rc.poison@gmail.com>
2018-09-28 00:40:15 +0200

    test

--- dxvk-cache-pool-client/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/client/DxvkCachePoolClient.java
+++ dxvk-cache-pool-client/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/client/DxvkCachePoolClient.java
@@ -5,6 +5,14 @@
  */
 package com.ignorelist.kassandra.dxvk.cache.pool.client;
 
+import com.ignorelist.kassandra.dxvk.cache.pool.client.rest.DxvkCachePoolRestClient;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.DxvkStateCacheIO;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCache;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 /**
  *
  * @author poison
@@ -14,8 +22,15 @@ public class DxvkCachePoolClient {
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
+	public static void main(String[] args) throws IOException {
+		final Path path=Paths.get("/usr/local/games/SteamLibrary/Steam/SteamApps/common/Beat Saber/Beat Saber.dxvk-cache");
+		DxvkStateCache cache=DxvkStateCacheIO.parse(path);
+		try (DxvkCachePoolRestClient restClient=new DxvkCachePoolRestClient("http://localhost:16969")) {
+			restClient.store(cache);
+			DxvkStateCache stateCache=restClient.getStateCache(cache.getVersion(), cache.getExecutableInfo());
+			System.err.println(Objects.equals(cache, stateCache));
+		}
+		
 	}
 
 }
