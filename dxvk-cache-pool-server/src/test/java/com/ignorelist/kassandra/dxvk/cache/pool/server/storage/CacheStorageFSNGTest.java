26dda3677536ce6c157c74c57b776d5cad2b8cc3
poison <rc.poison@gmail.com>
2018-09-29 19:52:56 +0200

    more tests for storage

--- dxvk-cache-pool-server/src/test/java/com/ignorelist/kassandra/dxvk/cache/pool/server/storage/CacheStorageFSNGTest.java
+++ dxvk-cache-pool-server/src/test/java/com/ignorelist/kassandra/dxvk/cache/pool/server/storage/CacheStorageFSNGTest.java
@@ -9,12 +9,15 @@ import com.google.common.collect.ImmutableSet;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.DxvkStateCacheIO;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCache;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCacheEntry;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCacheEntryInfo;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.DxvkStateCacheInfo;
 import com.ignorelist.kassandra.dxvk.cache.pool.test.TestUtil;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.UUID;
 import static org.testng.Assert.*;
@@ -72,18 +75,39 @@ public class CacheStorageFSNGTest {
 	}
 
 	@Test(dependsOnMethods={"testStore"})
-	public void testGetMissingEntries() throws IOException {
+	public void testGetMissingEntriesMissingNone() throws IOException {
 		DxvkStateCacheInfo existingCache=cache.copy().toInfo();
-
 		try (CacheStorageFS instance=new CacheStorageFS(storagePath)) {
 			Set<DxvkStateCacheEntry> missingEntries=instance.getMissingEntries(existingCache);
 			assertTrue(missingEntries.isEmpty());
 
+		}
+	}
+
+	@Test(dependsOnMethods={"testStore"})
+	public void testGetMissingEntriesMissingAll() throws IOException {
+		try (CacheStorageFS instance=new CacheStorageFS(storagePath)) {
 			DxvkStateCacheInfo empty=cache.copy().toInfo();
 			empty.setEntries(ImmutableSet.of());
 			Set<DxvkStateCacheEntry> missingEntriesForEmpty=instance.getMissingEntries(empty);
-			System.err.println(missingEntriesForEmpty.size());
 			assertEquals(missingEntriesForEmpty, cache.getEntries());
+		}
+	}
+
+	@Test(dependsOnMethods={"testStore"})
+	public void testGetMissingEntriesMissingPartial() throws IOException {
+		DxvkStateCacheInfo existingCache=cache.copy().toInfo();
+		Set<DxvkStateCacheEntryInfo> entries=new HashSet<>(existingCache.getEntries());
+		Iterator<DxvkStateCacheEntryInfo> iterator=entries.iterator();
+		DxvkStateCacheEntryInfo missing=iterator.next();
+		iterator.remove();
+		existingCache.setEntries(entries);
+		try (CacheStorageFS instance=new CacheStorageFS(storagePath)) {
+			Set<DxvkStateCacheEntryInfo> missingEntries=instance.getMissingEntries(existingCache).stream()
+					.map(DxvkStateCacheEntry::getDescriptor)
+					.collect(ImmutableSet.toImmutableSet());
+			assertEquals(missingEntries, ImmutableSet.of(missing));
+			assertEquals(missingEntries.size(), 1);
 		}
 	}
 
