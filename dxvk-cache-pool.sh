1c8345c8cdb686df34f9cb878bf17c63d482af9f
poison <rc.poison@gmail.com>
2018-09-30 06:52:22 +0200

    fix env var

--- dxvk-cache-pool.sh
+++ dxvk-cache-pool.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#export DXVK_STATE_CACHE_PATH="$XDG_CACHE_HOME/dxvk" # $XDG_CACHE_HOME is not yet available when sourcing this ins /etc/profile.d
-export DXVK_STATE_CACHE_PATH="$HOME/.cache/dxvk"
+export DXVK_STATE_CACHE_PATH="/dxvk-cache-pool"
 
-[ ! -d "$DXVK_STATE_CACHE_PATH" ] && mkdir -p "$DXVK_STATE_CACHE_PATH"
