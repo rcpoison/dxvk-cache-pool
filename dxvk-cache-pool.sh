8dfaeb44478ae267c81ebdb6f0bafa6eafe2dec2
poison <rc.poison@gmail.com>
2018-09-30 02:00:54 +0200

    ...

--- dxvk-cache-pool.sh
+++ dxvk-cache-pool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #export DXVK_STATE_CACHE_PATH="$XDG_CACHE_HOME/dxvk" # $XDG_CACHE_HOME is not yet available when sourcing this ins /etc/profile.d
-export DXVK_STATE_CACHE_PATH="~/.cache/dxvk"
+export DXVK_STATE_CACHE_PATH="$HOME/.cache/dxvk"
 
 [ ! -d "$DXVK_STATE_CACHE_PATH" ] && mkdir -p "$DXVK_STATE_CACHE_PATH"
