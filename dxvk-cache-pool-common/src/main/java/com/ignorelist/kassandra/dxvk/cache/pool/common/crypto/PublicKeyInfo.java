d9be0246cf57ad3cfc8518f3d11791745e39e5e0
poison <rc.poison@gmail.com>
2018-10-01 17:19:51 +0200

    model for pubkey info

---
+++ dxvk-cache-pool-common/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/common/crypto/PublicKeyInfo.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ignorelist.kassandra.dxvk.cache.pool.common.crypto;
+
+import com.google.common.hash.Hashing;
+import java.util.Arrays;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAttribute;
+
+/**
+ *
+ * @author poison
+ */
+public class PublicKeyInfo {
+
+	private byte[] hash;
+
+	public PublicKeyInfo() {
+	}
+
+	public PublicKeyInfo(byte[] hash) {
+		this.hash=hash;
+	}
+	
+	public PublicKeyInfo(PublicKey publicKey) {
+		this(Hashing.sha256().hashBytes(publicKey.getKey()).asBytes());
+	}
+
+	@NotNull
+	@Size(min=32, max=32)
+	@XmlAttribute(required=true)
+	public byte[] getHash() {
+		return hash;
+	}
+
+	public void setHash(byte[] hash) {
+		this.hash=hash;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash=7;
+		hash=37*hash+Arrays.hashCode(this.hash);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this==obj) {
+			return true;
+		}
+		if (obj==null) {
+			return false;
+		}
+		if (getClass()!=obj.getClass()) {
+			return false;
+		}
+		final PublicKeyInfo other=(PublicKeyInfo) obj;
+		if (!Arrays.equals(this.hash, other.hash)) {
+			return false;
+		}
+		return true;
+	}
+
+}
