d788ea3df960057993793a237469581798bb8996
poison <rc.poison@gmail.com>
2018-10-03 02:20:47 +0200

    bind CacheStorageSigned

--- dxvk-cache-pool-server/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/server/ServerBinder.java
+++ dxvk-cache-pool-server/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/server/ServerBinder.java
@@ -1,7 +1,9 @@
 package com.ignorelist.kassandra.dxvk.cache.pool.server;
 
 import com.ignorelist.kassandra.dxvk.cache.pool.common.api.CacheStorage;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.api.CacheStorageSigned;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.api.SignatureStorage;
+import com.ignorelist.kassandra.dxvk.cache.pool.server.storage.CacheStorageSignedFacade;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 /**
@@ -14,11 +16,13 @@ public class ServerBinder extends AbstractBinder {
 	private final Configuration configuration;
 	private final CacheStorage cacheStorage;
 	private final SignatureStorage signatureStorage;
+	private final CacheStorageSigned cacheStorageSigned;
 
 	public ServerBinder(final Configuration configuration, final CacheStorage cacheStorage, final SignatureStorage signatureStorage) {
 		this.configuration=configuration;
 		this.cacheStorage=cacheStorage;
 		this.signatureStorage=signatureStorage;
+		cacheStorageSigned=new CacheStorageSignedFacade(cacheStorage, signatureStorage);
 	}
 
 	@Override
@@ -26,6 +30,7 @@ public class ServerBinder extends AbstractBinder {
 		bind(configuration).to(Configuration.class);
 		bind(cacheStorage).to(CacheStorage.class);
 		bind(signatureStorage).to(SignatureStorage.class);
+		bind(cacheStorageSigned).to(CacheStorageSigned.class);
 	}
 
 }
