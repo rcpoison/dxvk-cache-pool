240628d3e050f31ca8ccb5d259859a4e35ba6cca
poison <rc.poison@gmail.com>
2018-10-03 15:43:04 +0200

    validation for signed cache

--- dxvk-cache-pool-common/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/common/model/validators/StateCacheValidator.java
+++ dxvk-cache-pool-common/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/common/model/validators/StateCacheValidator.java
@@ -8,6 +8,9 @@ package com.ignorelist.kassandra.dxvk.cache.pool.common.model.validators;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.StateCacheHeaderInfo;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.Util;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCache;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheEntrySigned;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheMeta;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheSigned;
 
 /**
  *
@@ -15,11 +18,19 @@ import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCache;
  */
 public class StateCacheValidator {
 
-	public void validate(StateCache cache) {
+	public void validateMeta(StateCacheMeta cache) {
 		final int entrySize=StateCacheHeaderInfo.getEntrySize(cache.getVersion());
 		if (entrySize!=cache.getEntrySize()) {
 			throw new IllegalArgumentException("expected entrySize:"+entrySize+", got:"+cache.getEntrySize());
 		}
+		if (!Util.isSafeBaseName(cache.getBaseName())) {
+			throw new IllegalArgumentException("illegal baseName: "+cache.getBaseName());
+		}
+	}
+
+	public void validate(StateCache cache) {
+		validateMeta(cache);
+		final int entrySize=StateCacheHeaderInfo.getEntrySize(cache.getVersion());
 		boolean allValidLength=cache.getEntries().stream()
 				.mapToInt(e -> e.getEntry().length)
 				.allMatch(l -> l==entrySize);
@@ -27,8 +38,17 @@ public class StateCacheValidator {
 			throw new IllegalArgumentException("illegal entry size found");
 		}
 
-		if (!Util.isSafeBaseName(cache.getBaseName())) {
-			throw new IllegalArgumentException("illegal baseName: "+cache.getBaseName());
+	}
+
+	public void validate(StateCacheSigned cache) {
+		validateMeta(cache);
+		final int entrySize=StateCacheHeaderInfo.getEntrySize(cache.getVersion());
+		boolean allValidLength=cache.getEntries().stream()
+				.map(StateCacheEntrySigned::getCacheEntry)
+				.mapToInt(e -> e.getEntry().length)
+				.allMatch(l -> l==entrySize);
+		if (!allValidLength) {
+			throw new IllegalArgumentException("illegal entry size found");
 		}
 
 	}
