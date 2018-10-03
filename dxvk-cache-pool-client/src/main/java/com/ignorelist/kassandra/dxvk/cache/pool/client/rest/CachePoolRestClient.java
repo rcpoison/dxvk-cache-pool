95e7599d9bacfc84d17a0b08dc30053bd2b86a18
poison <rc.poison@gmail.com>
2018-10-03 17:25:24 +0200

    implement CacheStorageSigned in REST client

--- dxvk-cache-pool-client/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/client/rest/CachePoolRestClient.java
+++ dxvk-cache-pool-client/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/client/rest/CachePoolRestClient.java
@@ -6,9 +6,14 @@
 package com.ignorelist.kassandra.dxvk.cache.pool.client.rest;
 
 import com.ignorelist.kassandra.dxvk.cache.pool.common.api.CacheStorage;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.api.CacheStorageSigned;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCache;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheEntry;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheEntrySigned;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheInfo;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheInfoSignees;
+import com.ignorelist.kassandra.dxvk.cache.pool.common.model.StateCacheSigned;
+import java.io.IOException;
 import java.util.Set;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
@@ -19,7 +24,7 @@ import javax.ws.rs.core.MediaType;
  *
  * @author poison
  */
-public class CachePoolRestClient extends AbstractRestClient implements CacheStorage {
+public class CachePoolRestClient extends AbstractRestClient implements CacheStorage, CacheStorageSigned {
 
 	private static final String PATH="pool";
 
@@ -27,7 +32,11 @@ public class CachePoolRestClient extends AbstractRestClient implements CacheStor
 	};
 	private static final GenericType<Set<StateCacheInfo>> TYPE_CACHE_INFO_SET=new GenericType<Set<StateCacheInfo>>() {
 	};
+	private static final GenericType<Set<StateCacheInfoSignees>> TYPE_CACHE_INFO_SIGNEES_SET=new GenericType<Set<StateCacheInfoSignees>>() {
+	};
 	private static final GenericType<Set<StateCacheEntry>> TYPE_CACHE_ENTRY_SET=new GenericType<Set<StateCacheEntry>>() {
+	};
+	private static final GenericType<Set<StateCacheEntrySigned>> TYPE_CACHE_ENTRY_SIGNED_SET=new GenericType<Set<StateCacheEntrySigned>>() {
 	};
 
 	public CachePoolRestClient(String baseUrl) {
@@ -89,6 +98,48 @@ public class CachePoolRestClient extends AbstractRestClient implements CacheStor
 				.path(subString)
 				.request(MediaType.APPLICATION_JSON)
 				.get(TYPE_STRING_SETn);
+	}
+
+	@Override
+	public StateCacheInfoSignees getCacheDescriptorSignees(int version, String baseName) {
+		return getWebTarget()
+				.path("cacheDescriptorSignees")
+				.path(Integer.toString(version))
+				.request(MediaType.APPLICATION_JSON)
+				.post(Entity.text(baseName), StateCacheInfoSignees.class);
+	}
+
+	public Set<StateCacheInfoSignees> getCacheDescriptorsSignees(int version, Set<String> baseNames) {
+		return getWebTarget()
+				.path("cacheDescriptorsSignees")
+				.path(Integer.toString(version))
+				.request(MediaType.APPLICATION_JSON)
+				.post(Entity.json(baseNames), TYPE_CACHE_INFO_SIGNEES_SET);
+	}
+
+	@Override
+	public StateCacheSigned getCacheSigned(int version, String baseName) {
+		return getWebTarget()
+				.path("stateCacheSigned")
+				.path(Integer.toString(version))
+				.request(MediaType.APPLICATION_JSON)
+				.post(Entity.json(baseName), StateCacheSigned.class);
+	}
+
+	@Override
+	public Set<StateCacheEntrySigned> getMissingEntriesSigned(StateCacheInfo cacheInfo) {
+		return getWebTarget()
+				.path("missingCacheEntriesSigned")
+				.request(MediaType.APPLICATION_JSON)
+				.post(Entity.json(cacheInfo), TYPE_CACHE_ENTRY_SIGNED_SET);
+	}
+
+	@Override
+	public void storeSigned(StateCacheSigned cache) throws IOException {
+		getWebTarget()
+				.path("store")
+				.request()
+				.post(Entity.json(cache));
 	}
 
 }
