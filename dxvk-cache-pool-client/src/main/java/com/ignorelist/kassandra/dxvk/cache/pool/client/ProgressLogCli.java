2bb6d09ce62ea8d60757d6045faa88dad107a5eb
poison <rc.poison@gmail.com>
2018-10-06 01:56:38 +0200

    log implementation for serr

---
+++ dxvk-cache-pool-client/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/client/ProgressLogCli.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ignorelist.kassandra.dxvk.cache.pool.client;
+
+import com.ignorelist.kassandra.dxvk.cache.pool.common.api.ProgressLog;
+
+/**
+ *
+ * @author poison
+ */
+public class ProgressLogCli implements ProgressLog {
+
+	@Override
+	public void log(Level level, CharSequence message) {
+		log(level, null, message);
+	}
+
+	@Override
+	public void log(Level level, CharSequence prefix, CharSequence message) {
+		if (Level.SUB==level) {
+			System.err.print(" -> ");
+		} else if (Level.WANING==level) {
+			System.err.print("Warning: ");
+		} else if (Level.ERROR==level) {
+			System.err.print("Error: ");
+		}
+		if (null!=prefix) {
+			System.err.print(prefix);
+			System.err.print(": ");
+		}
+		System.err.println(message);
+	}
+
+}
