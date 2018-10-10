30ae03f8fc361090b5492caf7ee4436ba0284bcd
poison <rc.poison@gmail.com>
2018-10-10 22:09:48 +0200

    variant that allows passing existing info

--- dxvk-cache-pool-server/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/server/storage/CacheStorageSignedFacade.java
+++ dxvk-cache-pool-server/src/main/java/com/ignorelist/kassandra/dxvk/cache/pool/server/storage/CacheStorageSignedFacade.java
@@ -12,7 +12,7 @@ import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Multiset;
+import com.google.common.collect.SortedMultiset;
 import com.google.common.collect.TreeMultiset;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.api.CacheStorage;
 import com.ignorelist.kassandra.dxvk.cache.pool.common.api.SignatureStorage;
@@ -149,11 +149,15 @@ public class CacheStorageSignedFacade implements CacheStorageSigned {
 		}
 	}
 
-	public Multiset<Integer> buildSignatureCountStats(final int version, final String baseName) {
-		return cacheStorage.getCacheDescriptor(version, baseName).getEntries().stream()
+	public SortedMultiset<Integer> buildSignatureCountStats(StateCacheInfo stateCacheInfo) {
+		return stateCacheInfo.getEntries().stream()
 				.map(signatureStorage::getSignedBy)
 				.map(Set::size)
 				.collect(Collectors.toCollection(TreeMultiset::create));
+	}
+
+	public SortedMultiset<Integer> buildSignatureCountStats(final int version, final String baseName) {
+		return buildSignatureCountStats(cacheStorage.getCacheDescriptor(version, baseName));
 	}
 
 	@Override
