8eb96aecaf454c60730e6168121a52a91a9c3444
poison <rc.poison@gmail.com>
2018-10-11 16:11:30 +0200

    tooltip on download link

--- dxvk-cache-pool-server/src/main/resources/com/ignorelist/kassandra/dxvk/cache/pool/server/rest/js/main.js
+++ dxvk-cache-pool-server/src/main/resources/com/ignorelist/kassandra/dxvk/cache/pool/server/rest/js/main.js
@@ -1,24 +1,24 @@
 
 $(document).ready(function () {
 
-	$('li.entry a.entryCount').tooltip({
+	$('li.entry a.download,li.entry span.entryCount').tooltip({
 		content: function (callback) {
 			var element = $(this);
-			var baseName = $(this).parent().attr('data-baseName');
+			var baseName = $(this).closest('li').attr('data-baseName');
 			$.ajax({
 				type: 'GET',
 				dataType: 'json',
 				cache: true,
 				url: '/signatureStats/' + baseName,
 				beforeSend: function (xhr) {
-					if ($(element).data('stats')) {
+					if ($(element).closest('li').data('stats')) {
 						callback(buildStatsTooltip(element));
 						return false;
 					}
 					return true;
 				},
 				success: function (stats) {
-					$(element).data('stats', stats);
+					$(element).closest('li').data('stats', stats);
 					callback(buildStatsTooltip(element));
 				}
 			});
@@ -30,7 +30,7 @@ $(document).ready(function () {
 
 function buildStatsTooltip(element) {
 	var table = '<table class="stats"><tr><th>Signature count</th><th>Occurences</th></tr>';
-	$.each($(element).data('stats'), function (k, v) {
+	$.each($(element).closest('li').data('stats'), function (k, v) {
 		table += '<tr><td>' + v.signatureCount + '</td><td>' + v.occurences + '</td></tr>';
 	});
 	table += '</table';
