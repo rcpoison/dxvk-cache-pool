644d89a0ef6d99457a05d6ea52e6ad61f18c0820
poison <rc.poison@gmail.com>
2018-11-03 16:26:51 +0100

    remove pointless test

--- dxvk-cache-pool.sh
+++ dxvk-cache-pool.sh
@@ -3,4 +3,4 @@
 # $XDG_CACHE_HOME isn't available here, assume it's set to its default value
 export DXVK_STATE_CACHE_PATH="$HOME/.cache/dxvk-cache-pool"
 
-[ ! -d "$DXVK_STATE_CACHE_PATH" ] && mkdir -p "$DXVK_STATE_CACHE_PATH"
+mkdir -p "$DXVK_STATE_CACHE_PATH"
